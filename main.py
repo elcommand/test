2863239eb77a63e94482e38dc26a6811494f39b9
LinkUp <jbrzustowski@gmail.com>
2020-10-15 00:26:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# hostname = input("Enter host IP address: ")
+# username = input("Enter SSH Username: ")
+# password = input("Enter SSH Password: ")
+
+hostname = '192.168.2.1'
+username = 'admin'
+password = '******'
+port = 22
+
+ssh.connect(hostname, port, username, password, look_for_keys=False)
+stdin,stdout,stderr = ssh.exec_command('/ip arp print')
+output = stdout.readlines()
+print (''.join(output))
